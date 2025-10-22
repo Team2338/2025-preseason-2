1ada0d5373e8686ccd13a7074e1353770bf0c483
gitanjali <sgitanjali3112@gmail.com>
2025-10-21 20:58:17 -0500

    created drive train with arcade control

---
+++ src/main/java/team/gif/robot/subsystems/DriveTrain.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package team.gif.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team.gif.robot.Robot;
+import team.gif.robot.RobotMap;
+
+public class DriveTrain extends SubsystemBase {
+
+    private WPI_TalonSRX leftMotor;
+    private WPI_TalonSRX rightMotor;
+    private DifferentialDrive drive;
+
+
+    public DriveTrain() {
+        leftMotor = new WPI_TalonSRX(RobotMap.LEFT_DRIVETRAIN_TALON_ID);
+        rightMotor = new WPI_TalonSRX(RobotMap.RIGHT_DRIVETRAIN_TALON_ID);
+
+        leftMotor.configFactoryDefault();
+        rightMotor.configFactoryDefault();
+
+        leftMotor.setNeutralMode(NeutralMode.Brake);
+        rightMotor.setNeutralMode(NeutralMode.Brake);
+
+        drive = new DifferentialDrive(leftMotor, rightMotor);
+
+
+    }
+
+    public void arcadeDrive(double speed, double rotation){
+        drive.arcadeDrive(speed, rotation);
+    }
+
+
+}
